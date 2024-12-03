5aa0ca59aae32baff4a86399f412387ef7362b58
voylin <0voylin0@gmail.com>
2024-12-03 12:11:33 +0900

    Day 01 fix

--- src/01/day_01.gd
+++ src/01/day_01.gd
@@ -2,42 +2,38 @@ extends Control
 
 
 
-func _ready() -> void:
-	print("Day 01 - Part one result: ", part_one())
-	print("Day 01 - Part two result: ", part_two())
-
-
 func part_one() -> int:
-	var data: Array[PackedInt64Array] = _read_data()
+	var data: Array[PackedInt64Array] = _get_split_data()
 	var total: int = 0
 
-	data[0].sort()
-	data[1].sort()
 
-	for i: int in data[0].size():
-		total += abs(data[0][i] - data[1][i])
+	for id: int in data[0].size():
+		total += abs(data[0][id] - data[1][id])
+
 	return total
 	
 
 func part_two() -> int:
-	var data: Array[PackedInt64Array] = _read_data()
+	var data: Array[PackedInt64Array] = _get_split_data()
 	var total: int = 0 # Similarity score 
 
 	for number: int in data[0]:
 		total += number * data[1].count(number)
+
 	return total
 
 
-func _read_data() -> Array[PackedInt64Array]:
-	var data: Array[PackedInt64Array] = [[],[]]
+func _get_split_data() -> Array[PackedInt64Array]:
+	var data: Array[PackedInt64Array] = Data.get_packed_int_array("res://01/data.txt", "   ")
+	var left: PackedInt64Array = []
+	var right: PackedInt64Array = []
 
-	var file: FileAccess = FileAccess.open("res://01/test_data.txt", FileAccess.READ)
-	var line: PackedStringArray = file.get_line().split("   ")
+	for entry: PackedInt64Array in data:
+		left.append(entry[0])
+		right.append(entry[1])
 
-	while !file.eof_reached():
-		data[0].append(int(line[0]))
-		data[1].append(int(line[1]))
-		line = file.get_line().split("   ")
+	left.sort()
+	right.sort()
 
-	return data
+	return [left, right]
 
